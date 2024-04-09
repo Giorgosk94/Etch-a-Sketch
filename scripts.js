65423a120d889738849589b71cdae6702ba9e5fa
geo kouk <giorgosks1994@gmail.com>
2024-04-09 22:35:05 +0300

    fisrt

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+const cdiv = document.querySelector('.container');
+var a = document.getElementById("fname").value;
+
+
+
+for(x=0; x<(a*a);x++) {
+
+
+    
+    var div = document.createElement("div");
+div.style.width = "10px";
+div.style.height = "10px";
+div.style.background = "white";
+div.style.color = "black";
+div.style.border="1px solid";
+cdiv.appendChild(div);
+    
+   
+    
+    
+}
